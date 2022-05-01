7556e481271e1d5162b902b5a82996c6f88e4a82
Vikram Srinivas <73585468+vikram2009@users.noreply.github.com>
2022-05-01 12:48:01 +0530

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+string followG;
+class Ginyos{
+  pubic:
+  void ask(){
+    cout << "DID U FOLLOW GINYOS";
+    cin >> followG;
+  }
+}
+
+int main(){
+Ginyos y;
+  y.aks();
+
+}
